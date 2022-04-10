dd487973d0462b9b2520ac047fe9cea22d71cc5e
liuding <liuding.ld@foxmail.com>
2022-04-10 19:17:52 +0800

    data

--- src/scripts/optimize.js
+++ src/scripts/optimize.js
@@ -2,33 +2,51 @@ const sharp = require(`sharp`)
 const glob = require(`glob`)
 const fs = require(`fs-extra`)
 
-const matches = glob.sync(`src/images/**/*.{png,jpg,jpeg}`).concat(glob.sync(`static/images/**/*.{png,jpg,jpeg}`))
-const MAX_WIDTH = 1800
+const matches = glob.sync(`static/images/**/*.{png,jpg,jpeg}`)
+const MAX_WIDTH = 512
 const QUALITY = 70
 
 console.log(`Found ${matches.length} images`)
 
 Promise.all(
   matches.map(async match => {
-    const stream = sharp(match)
-    const info = await stream.metadata()
+    try {
+      const stream = sharp(match)
+      const info = await stream.metadata()
 
-    // if (info.width < MAX_WIDTH) {
-    //   return
-    // }
+      if (info.width < MAX_WIDTH || match.indexOf('-optimized') > 0) {
+        return
+      }
 
-    const optimizedName = match.replace(
-      /(\..+)$/,
-      (match, ext) => `-optimized${ext}`
-    )
+      const optimizedName = match.replace(
+        /(\..+)$/,
+        (match, ext) => `-optimized${ext}`
+      )
 
-    console.log(`Optimizing ${match}`, optimizedName)
+      console.log(`Optimizing ${match}`, optimizedName)
 
-    await stream
-      .resize(MAX_WIDTH)
-      .jpeg({ quality: QUALITY })
-      .toFile(optimizedName)
+      if (match.indexOf('.png') > 0) {
+        await stream
+          .resize(MAX_WIDTH)
+          .png({
+            quality: QUALITY
+          })
+          .toFile(optimizedName)
+      } else {
+        await stream
+          .resize(MAX_WIDTH)
+          .jpeg({
+            quality: QUALITY
+          })
+          .toFile(optimizedName)
+      }
 
-    return fs.rename(optimizedName, match)
+      console.log(`Write back: ${match}`)
+      return fs.rename(optimizedName, match)
+
+    } catch (err) {
+      console.error(err)
+    }
+
   })
 )
